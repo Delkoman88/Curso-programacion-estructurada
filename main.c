1c2a9b990df93b1cc053d50a3c3b7d8c081aa00e
Edgar Mancilla <delkoman@gmail.com>
2020-02-05 21:33:00 -0800

    ingreso de datos en variables por el teclado y mostrar en pantalla

--- main.c
+++ main.c
@@ -4,11 +4,35 @@
 
 int main()
 {
-    int integerA = 1;
-    float floatA = 1.1;
-    char letterA = 'A';
-    bool boolean = false;
+    int integerA;
+    float floatA;
+    double doubleA;
+    char letterA;
 
+    printf("Ingresa el valor del entero A: ");
+
+    scanf("%i", &integerA);
+
+    printf("Ingresa el valor del float A: ");
+
+    scanf("%f", &floatA);
+
+    printf("Ingresa el valor del double A: ");
+
+    scanf("%f", &doubleA);
+
+    printf("Ingresa el valor del caracter A: ");
+
+    scanf(" %c", &letterA);
+
+    //En la siguiente linea estamos imprimiendo mi variable integerA
+    printf("El valor del numero entero A es: %i \n", integerA);
+
+    printf("El valor del flotante A es: %f \n", floatA);
+
+    printf("El valor del double A es: %f \n", doubleA);
+
+    printf("El valor del caracter A es: %c \n", letterA);
 
     return 0;
 }
